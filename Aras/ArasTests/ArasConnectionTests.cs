0368c4b4693b5ae8c8a4832064bfc9e4aed0d791
Tomas Anderssom <tomas.j.andersson@gmail.com>
2023-03-24 19:48:39 +0100

    wip: Concept of Arrange wrapper to separate test failed in during arrange phase

--- Aras/ArasTests/ArasConnectionTests.cs
+++ Aras/ArasTests/ArasConnectionTests.cs
@@ -1,4 +1,5 @@
 using ArasTests.Setup;
+using System;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -17,6 +18,63 @@ namespace ArasTests
             Assert.False(string.IsNullOrEmpty(loggedInUserId), "There should be a user id");
             output.WriteLine(fixture.GetAdminInn().getUserID());
         }
+
+        [Fact]
+        public void Test() {
+            // Arrange
+            string val = string.Empty;
+            Action arrange = () => { 
+                val = "Hepp";
+                int i = 0;
+                int j =3;
+                int k = j/i;
+            };
+            Arranger.Run(arrange);
+            //Arranger.SetAction(arrangeAction).Run();
+
+            // Act
+            val = val.ToUpper();
+
+            //Assert
+            Assert.Equal("HEPP", val);
+        }
+
+        private class Arranger {
+            private Action _action;
+            public Arranger(Action action) {
+                _action = action;
+            }
+
+            public static Arranger SetAction(Action action) {
+                return new Arranger(action);
+            }
+
+            public void Run() {
+                try {
+                    _action();
+                }
+                catch (Exception ex) {
+                    throw new ArrangeException("Arrange exception: ", ex);
+                }
+            }
+
+            public static void Run(Action action) {
+                try {
+                    action();
+                }
+                catch (Exception ex) {
+                    throw new ArrangeException("Arrange exception: ", ex);
+                }
+            }
+        }
+
+        public class ArrangeException : Exception {
+            public ArrangeException(string message, Exception ex) : base(message, ex) {
+               
+            }
+        }
+
+
     }
 
 
